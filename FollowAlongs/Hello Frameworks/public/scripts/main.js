b9e9b6d85abd7227534db94621637302642875eb
Peters-Joseph <petersjl@rose-hulman.edu>
2020-09-16 15:22:27 -0400

    button for loops

--- FollowAlongs/Hello Frameworks/public/scripts/main.js
+++ FollowAlongs/Hello Frameworks/public/scripts/main.js
@@ -10,27 +10,30 @@
 var rhit = rhit || {};
 
 /** globals */
-rhit.variableName = "";
+rhit.counter = 0;
 
 /** function and class syntax examples */
 rhit.functionName = function () {
 	/** function body */
 };
 
-rhit.ClassName = class {
-	constructor() {
-
-	}
-
-	methodName() {
-
-	}
-}
-
 /* Main */
 /** function and class syntax examples */
 rhit.main = function () {
 	console.log("Ready");
+	const buttons = document.querySelectorAll("#counterButtons button");
+	// for (let i = 0; i < buttons.length; i++){
+	// 	let button = buttons[i];
+	// 	button.onclick = (event) => {
+	// 		console.log(`You pressed`, button)
+	// 	}
+	// }
+	
+	for (const button of buttons) {
+		button.onclick = (event) => {
+			console.log(`You pressed`, button)
+		}
+	}
 };
 
 rhit.main();
