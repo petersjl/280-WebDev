a825a2e6cddd62b8268082d3cc7a68018116ce89
Peters-Joseph <petersjl@rose-hulman.edu>
2020-09-24 23:20:45 -0400

    finished js

--- Homeworks/Favorite Things/public/scripts/main.js
+++ Homeworks/Favorite Things/public/scripts/main.js
@@ -1,36 +1,48 @@
-/**
- * @fileoverview
- * Provides the JavaScript interactions for all pages.
- *
- * @author 
- * PUT_YOUR_NAME_HERE
- */
-
-/** namespace. */
 var rhit = rhit || {};
 
-/** globals */
-rhit.variableName = "";
-
-/** function and class syntax examples */
-rhit.functionName = function () {
-	/** function body */
-};
-
-rhit.ClassName = class {
-	constructor() {
-
-	}
-
-	methodName() {
-
-	}
-}
-
-/* Main */
-/** function and class syntax examples */
+rhit.counter = document.querySelector("#numberCounter");
+console.log(rhit.counter);
+rhit.count = 0;
+rhit.colorBox = document.querySelector("#favoriteColorBox");
+console.log(rhit.colorBox);
+rhit.color = "#800000";
 rhit.main = function () {
 	console.log("Ready");
+	document.querySelector("#buttonAdd").onclick = (event) => {
+		console.log("clicked add");
+		rhit.count++;
+		rhit.counter.innerHTML = rhit.count;
+	}
+	document.querySelector("#buttonReset").onclick = (event) => {
+		rhit.count = 0;
+		rhit.counter.innerHTML = rhit.count;
+	}
+	document.querySelector("#buttonSub").onclick = (event) => {
+		rhit.count--;
+		rhit.counter.innerHTML = rhit.count;
+	}
+	document.querySelector("#buttonBlue").onclick = (event) => {
+		rhit.color = "blue";
+		rhit.colorBox.style.backgroundColor = rhit.color;
+		rhit.colorBox.innerHTML = rhit.color;
+	}
+	document.querySelector("#buttonGreen").onclick = (event) => {
+		rhit.color = "green";
+		rhit.colorBox.style.backgroundColor = rhit.color;
+		rhit.colorBox.innerHTML = rhit.color;
+	}
+	document.querySelector("#buttonRed").onclick = (event) => {
+		rhit.color = "red";
+		rhit.colorBox.style.backgroundColor = rhit.color;
+		rhit.colorBox.innerHTML = rhit.color;
+	}
+	document.querySelector("#buttonPurple").onclick = (event) => {
+		rhit.color = "purple";
+		rhit.colorBox.style.backgroundColor = rhit.color;
+		rhit.colorBox.innerHTML = rhit.color;
+	}
 };
 
 rhit.main();
+
+
