f62c6a787c4840d22028ecaa9ddb952cae6a4d54
Peters-Joseph <petersjl@rose-hulman.edu>
2020-10-16 10:06:11 -0400

    finished email password sign in

--- FollowAlongs/FirebaseAuth/public/scripts/main.js
+++ FollowAlongs/FirebaseAuth/public/scripts/main.js
@@ -5,17 +5,34 @@ var rhit = rhit || {};
 rhit.main = function () {
 	console.log("Ready");
 
+	firebase.auth().onAuthStateChanged((user) =>{
+		if (user) {
+			console.log("The user is signed in " + user.uid);
+		}else{
+			console.log("There is no user signed in")
+		}
+	})
+
 	const inputEmailEl = document.getElementById("inputEmail");
 	const inputPasswordEl = document.getElementById("inputPassword");
 
 	document.getElementById("signOutButton").onclick = (event) => {
 		console.log("Sign out")
+		firebase.auth().signOut().then(() => {
+			console.log("User has been signed out");
+		});
 	}
 	document.getElementById("createAccountButton").onclick = (event) => {
 		console.log(`Create account for email: ${inputEmailEl.value} password: ${inputPasswordEl.value}`)
+		firebase.auth().createUserWithEmailAndPassword(inputEmailEl.value, inputPasswordEl.value).catch((error) => {
+			console.log("Error code: " + error.code + " " + error.message);
+		})
 	}
 	document.getElementById("logInButton").onclick = (event) => {
 		console.log(`Log in for email: ${inputEmailEl.value} password: ${inputPasswordEl.value}`)
+		firebase.auth().signInWithEmailAndPassword(inputEmailEl.value, inputPasswordEl.value).catch((error) => {
+			console.log("Error code: " + error.code + " " + error.message);
+		})
 	}
 };
 
