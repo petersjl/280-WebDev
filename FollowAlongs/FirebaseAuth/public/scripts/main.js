17ee1cdf7a6c003c219a395b6c0135e4b6a2ed83
Peters-Joseph <petersjl@rose-hulman.edu>
2020-10-16 10:23:54 -0400

    finished firebase signin

--- FollowAlongs/FirebaseAuth/public/scripts/main.js
+++ FollowAlongs/FirebaseAuth/public/scripts/main.js
@@ -8,6 +8,10 @@ rhit.main = function () {
 	firebase.auth().onAuthStateChanged((user) =>{
 		if (user) {
 			console.log("The user is signed in " + user.uid);
+			console.log("display name: " + user.displayName);
+			console.log("email: " + user.email);
+			console.log("phone number: " + user.phoneNumber);
+			console.log("isAnonymous: " + user.isAnonymous);
 		}else{
 			console.log("There is no user signed in")
 		}
@@ -34,6 +38,34 @@ rhit.main = function () {
 			console.log("Error code: " + error.code + " " + error.message);
 		})
 	}
+
+	document.getElementById("anonymousAuthButton").onclick = (event) => {
+		console.log("Sign in anonymously");
+		firebase.auth().signInAnonymously().catch((error) => {
+			console.log("Error code: " + error.code + " " + error.message);
+		})
+	}
+
+	rhit.startFirebaseUI();
 };
+
+rhit.startFirebaseUI = function() {
+	var uiConfig = {
+        signInSuccessUrl: '/',
+        signInOptions: [
+          // Leave the lines as is for the providers you want to offer your users.
+          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+          firebase.auth.EmailAuthProvider.PROVIDER_ID,
+          firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+          firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
+        ]
+        
+      };
+
+      // Initialize the FirebaseUI Widget using Firebase.
+      const ui = new firebaseui.auth.AuthUI(firebase.auth());
+      // The start method will wait until the DOM is loaded.
+      ui.start('#firebaseui-auth-container', uiConfig);
+}
 
 rhit.main();
