471eddcb2ea0b44036a9a2fb13b65fcdd0895be9
Zehren <petersjl@rose-hulman>
2020-11-02 19:44:05 -0500

    routing

--- FollowAlongs/Node/SimpleServers/noExpress.js
+++ FollowAlongs/Node/SimpleServers/noExpress.js
@@ -3,29 +3,49 @@ let totalTracker = 0;
 
 const http = require('http');
 
-const mainHandler = function (request, response) {
+const abcHandler = function (request, response) {
+	response.write('<h1>ABC</h1>');
+}
 
-	if(request.url == "/favicon.ico" ) {
-		response.writeHead(200, {'Content-Type': 'image/x-con'} );
-		response.end();
-		return;
-	}
-
-	console.log( request.url );
-
-	if(request.url == "/abc") abcTracker++;
-	totalTracker++;
-
-	response.statusCode = 200;
-	response.setHeader('Content-Type', 'text/html');
+const xyzHandler = function (request, response) {
 	response.write('<!doctype html>\n');
 	response.write('<html>\n<head>\n');
 	response.write('<title> Dice Roller</title>\n');
 	response.write('</head>\n<body>\n');
 	response.write('<h1>Hello World!</h1>');
 	response.write('<div>abcTracker ='+ abcTracker+'</div>');
-	response.write('<div>titalTracker ='+ totalTracker+'</div>');
+	response.write('<div>totalTracker ='+ totalTracker+'</div>');
 	response.write('\n</body>\n</html>');
+}
+
+const faviconHandler = function (request, response) {
+	response.writeHead(200, {'Content-Type': 'image/x-con'} );
+	response.end();
+}
+
+const mainHandler = function (request, response) {
+
+	if(request.url == "/favicon.ico" ) {
+		faviconHandler(request, response)
+		return;
+	}
+
+	response.statusCode = 200;
+	response.setHeader('Content-Type', 'text/html');
+	console.log( request.url );
+
+	if(request.url == "/abc" ) {
+		abcTracker++;
+		abcHandler(request, response);
+	}
+
+	let re=/^\/xyz.*/;
+
+	if(re.test(request.url)) {
+		xyzHandler(request, response);
+	}
+
+	totalTracker++;
     response.end();
 }
 
