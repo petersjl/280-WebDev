3defc034e98b646664bc9d857e96ff678dfaf2bd
Zehren <petersjl@rose-hulman>
2020-11-08 16:22:16 -0500

    finished express servers with data

--- FollowAlongs/Node/SimpleServers/express.js
+++ FollowAlongs/Node/SimpleServers/express.js
@@ -1,7 +1,33 @@
 var express = require("express")
 var app = express();
 
+let data=[];
 let counter = 0
+
+const fs = require("fs");
+const serverSideStorage = "../data/db.json";
+
+fs.readFile(serverSideStorage, function(err, buf) {
+	if(err) {
+		console.log("error: ", err);
+	}else {
+		data = JSON.parse(buf.toString())
+		if(data.length != 0){
+			counter = data[data.length-1];
+		}
+	}
+	console.log("Data read from file");
+})
+
+function saveToServer(data){
+	fs.writeFile(serverSideStorage, JSON.stringify(data), function(err, buf) {
+		if(err) {
+			console.log("error: ", err);
+		}else {
+			console.log("Data saved successfully");
+		}
+	})
+}
 
 app.use('/static', express.static("public") );
 
@@ -39,6 +65,20 @@ app.get('/pug', function(req,res) {
 
 app.get('/pug/hello', function(req,res) {
 	res.render('hello', {title:"Hello Button", count: counter})
+})
+
+var bodyParser = require("body-parser");
+app.use('/pug/hello', bodyParser.urlencoded( {extended: false} ))
+
+app.post('/pug/hello', function(req,res) {
+	counter = req.body.count || counter;
+	data.push(counter);
+	saveToServer(data);
+	res.render('hello', {title:"Hello Button", count: counter})
+})
+
+app.get('/pug/history', function(req,res) {
+	res.render('history', {title:"Count History", data: data})
 })
 
 app.listen(3000); 
