82353ee047c822f1aab9a69876fae2692af921cc
Peters-Joseph <petersjl@rose-hulman.edu>
2020-11-10 15:13:29 -0500

    serverside code

--- FollowAlongs/Node/TTTNode/app.js
+++ FollowAlongs/Node/TTTNode/app.js
@@ -2,4 +2,26 @@ const express = require("express")
 
 var app = express();
 app.use(express.static("public"));
+
+app.get("/api/getmove/:board", (req, res) => {
+    const boardString = req.params.board;
+
+    const openLocations = getOpenLocations(boardString);
+
+    const moveSelected = openLocations[Math.floor(Math.random() * openLocations.length)];
+
+    res.send({"move": moveSelected});
+})
+
+function getOpenLocations(boardString) {
+    const openLocations = [];
+    for (var i = 0; i < boardString.length; i++) {
+      if (boardString.charAt(i) == '-') {
+        openLocations.push(i)
+      }
+    }
+    return openLocations;
+}
+   
+
 app.listen(3000);
